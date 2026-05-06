43500e0a50961b61f44c70b3662a635f50fbb45e
shuaibkhan <skhan.csit@gmail.com>
2026-05-06 23:11:33 +0500

    work with path module

---
+++ path_module.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+
+// console.log("__filename", __filename);
+// console.log("__dirname", __dirname);
+
+const file_path = path.join("folder", "students", "data.txt");
+// console.log(file_path);
+
+// console.log(path.parse(file_path));
+// console.log(path.resolve(file_path));
+console.log(path.sep);
+
+
+
+
