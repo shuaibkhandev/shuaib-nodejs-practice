5b5dcdf2d28c46fde0f5ffa72da6aa9dfc4e10a4
shuaibkhan <skhan.csit@gmail.com>
2026-05-17 19:01:49 +0500

    work with form

--- express/server.js
+++ express/server.js
@@ -2,19 +2,31 @@ import express from "express";
 const app = express();
 import path from "path";
 
-const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+// const homePagePath = path.join(import.meta.dirname, "public", "index.html");
 const staticPath = path.join(import.meta.dirname, "public");
 app.use(express.static(staticPath))
-
-app.get("/profile/:username/blog/:slug", (req, res)=>{
-    const slug = req.params.slug.replaceAll("-"," ")
-    const username = req.params.username.replaceAll("-"," ")
+app.use(express.urlencoded({extended:true}));
+// app.get("/profile/:username/blog/:slug", (req, res)=>{
+//     const slug = req.params.slug.replaceAll("-"," ")
+//     const username = req.params.username.replaceAll("-"," ")
     
-    res.send(` <h1>Article ${slug} by ${username}</h1>`)
+//     res.send(` <h1>Article ${slug} by ${username}</h1>`)
 
+// })
+
+// app.get('/contact', (req, res) => {
+//     console.log(req.query);
+//     // res.redirect("/");
+//     res.send("OK")
+// })
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+    // res.redirect("/");
+    res.send("OK")
 })
 
-console.log(globalThis.process.env.PORT)
+// console.log(globalThis.process.env.PORT)
 const PORT = process.env.PORT
 app.listen(PORT, ()=>{
     console.log(`Server start at port no ${PORT}`);
